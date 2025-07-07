2ac89a9258d4eb39ae42ab16af5d704387934d43
Kenward Terhemba <codewithkenward@gmail.com>
2025-07-07 16:10:55 +0100

    add a script that converts numbers to integers

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,7 +1,10 @@
 const { argv } = require('process');
 
+const input = argv[2];
+const num = parseInt(input, 10);
+
 if (isNaN(argv[2])) {
     console.log("Not a number");
 } else {
-    console.log("My number: " + argv[2]);
+    console.log("My number: " + num);
 }
