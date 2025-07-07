92660cb4343288bffee981f3c5083a527c65fe8d
Kenward Terhemba <codewithkenward@gmail.com>
2025-07-07 16:01:37 +0100

    a script that concatenates

--- 4-concat.js
+++ 4-concat.js
@@ -1,13 +1,9 @@
 const { argv } = require('process');
 
-if (argv[2]) {
-    console.log(argv[2]);
-}
-
-else if (argv[2] && argv[3]) {
-    console.log(argv[2] + " is " + argv[3]);
-
-}
-else {
-    console.log("undefined is undefined");
+if (argv[2] && argv[3]) {
+    console.log(`${argv[2]} is ${argv[3]}`);
+} else if (argv[2] && !argv[3]) {
+    console.log(`${argv[2]} is undefined`);
+} else {
+    console.log(`undefined is undefined`);
 }
