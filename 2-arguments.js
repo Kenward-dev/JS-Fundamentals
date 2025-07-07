01cd754d7627e55c20a31095b3a5e7f0dc8ffa7e
Kenward Terhemba <codewithkenward@gmail.com>
2025-07-07 14:33:50 +0100

    add script that checks number of arguments

---
+++ 2-arguments.js
@@ -0,0 +1,14 @@
+const { argv } = require('process');
+
+if (argv.length < 3) {
+    console.log("No argument");
+}
+
+if (argv.length === 3) {
+    console.log("Argument found");
+}
+
+if (argv.length > 3) {
+    console.log("Arguments found");
+}
+
